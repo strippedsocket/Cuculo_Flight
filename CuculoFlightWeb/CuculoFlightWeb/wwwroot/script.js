4c4a48465fa10c36c492ebe5c9b5d76d01a6df0c
carlotta concina <stripped.socket@gmail.com>
2018-03-07 12:39:14 +0100

    aggiunta display none alla fine

--- CuculoFlightWeb/CuculoFlightWeb/wwwroot/script.js
+++ CuculoFlightWeb/CuculoFlightWeb/wwwroot/script.js
@@ -4,7 +4,7 @@
     ["#31", "#32", "#33", "#34", "#35", "#36", "#37", "#38"],
     ["#41", "#42", "#43", "#44", "#45", "#46", "#47", "#48"],
     ["#51", "#52", "#53", "#54", "#55", "#56", "#57", "#58"]
-    
+
 ];
 
 /**CUCULO**/
@@ -32,15 +32,16 @@ $(document).ready(function () {
 var branchesList = [];
 //var pB = [];
 //poi dinamico
-function Branch(x,y, id) {
+function Branch(x, y, id) {
     this.X = 7;
     this.Y = y;
     this.Id = id;
-  
+
 }
 
 
 function generateBranches() {
+    var nOfLoop;
     var numberOfBranches = Math.floor((Math.random() * 3) + 1);
     for (var i = 0; i < numberOfBranches; i++) {
         var branch = new Branch();
@@ -48,33 +49,32 @@ function generateBranches() {
         var positionY = Math.floor((Math.random() * 4) + 0);
         branch.Y = positionY;
         var positionedBranches = $(gameBoard[branch.Y][branch.X]).append('<div class="branch"></div>');
-        branch.Id=positionedBranches.attr("id");
-       // pB.push(positionedBranches.attr("id"));
+        branch.Id = positionedBranches.attr("id");
+        // pB.push(positionedBranches.attr("id"));
     }
-   
 
-    
+
+
     moveBrances();
     return branchesList;
 }
 
 
 function moveBrances() {
-    setInterval(function ()
-    {
+    setInterval(function () {
         for (var b of branchesList) {
-            if (b.X == 0) {
-                b.X = b.X;//momentaneo
-            } else {
-                b.X -= 1;
-                $("#" + b.Id).children().prependTo($(gameBoard[b.Y][b.X]));
-                b.Id -= 1;
+            //if ($("#" + b.Id).has(".branch")) { continue; }
+                if (b.X == 0) {
+                    $("#" + b.Id).children().css("display", "none");//lo faccio sparire->gestire con scontro
+                } else {
+                    b.X -= 1;
+                    $("#" + b.Id).children().prependTo($(gameBoard[b.Y][b.X]));
+                    b.Id -= 1;
+                }
 
-            }
+              
         }
-            
-        
-        
+
     }, 2000);
 }
 generateBranches();
